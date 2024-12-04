0b19dc948e0d026a1954f6fd4bf8c8d79bfb503d
skylicia123 <aliciasantoscoimbra@gmail.com>
2024-12-04 11:23:41 -0400

    segunda aula

--- main.c
+++ main.c
@@ -1,11 +1,51 @@
 #include <stdio.h>
-
-float main( ) 
+int main ()
 {
-	int a;
-	int b;
-	scanf("%d", &b);
-	scanf("%d", &a);
-	printf("numero: %d", b);
-	return 0.2; 
+	int num;
+	printf("escolha um numero entre 1 e 7:\n");
+	scanf("%d",&num);
+	switch (num)
+	{
+		case 1:
+		{
+			printf("segundou");
+			break;
+		}
+		case 2:
+		{
+			printf("terça");
+			break;
+		}
+		case 3:
+		{
+			printf("quak");
+			break;
+		}
+		case 4:
+		{
+			printf("quin");
+			break;
+		}
+		case 5:
+		{
+			printf("sex");
+			break;
+		}
+		case 6:
+		{
+			printf("saba");
+			break;
+		}
+		case 7:
+		{
+			printf("domin");
+			break;
+		}
+		default: 
+		{
+			printf("tu é gay");
+			break;
+		}
+	}
+	return 0;
 }
