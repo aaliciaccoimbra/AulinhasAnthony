2d75d576ee9896345c3142523e53e2aa4ee8b2c4
skylicia123 <aliciasantoscoimbra@gmail.com>
2024-12-10 11:11:33 -0400

    salarios

--- main.c
+++ main.c
@@ -1,51 +1,26 @@
 #include <stdio.h>
 int main ()
 {
-	int num;
-	printf("escolha um numero entre 1 e 7:\n");
-	scanf("%d",&num);
-	switch (num)
+	float salario;
+	printf("qual o salario bruto:\n");
+	scanf("%f",&salario); // como faço para não aparecer um tanto de 0 após o ponto
+	//Tentei fazer com switc case mas fiquei na duvida de como colocar uma condição para cada caso
+	if (salario<=1903.98)
 	{
-		case 1:
-		{
-			printf("segundou");
-			break;
-		}
-		case 2:
-		{
-			printf("terça");
-			break;
-		}
-		case 3:
-		{
-			printf("quak");
-			break;
-		}
-		case 4:
-		{
-			printf("quin");
-			break;
-		}
-		case 5:
-		{
-			printf("sex");
-			break;
-		}
-		case 6:
-		{
-			printf("saba");
-			break;
-		}
-		case 7:
-		{
-			printf("domin");
-			break;
-		}
-		default: 
-		{
-			printf("tu é gay");
-			break;
-		}
+		printf("não preciso de esmola de pobre");
 	}
-	return 0;
+	else 
+	{
+		if (salario>=1903.99 && salario<2826.65)
+	}
+	
 }
+
+/*
+Até R$1.903,98: Isento.
+De R$1.903,99 a R$2.826,65: 7,5%.
+De R$2.826,66 a R$3.751,05: 15%.
+De R$3.751,06 a R$4.664,68: 22,5%.
+Acima de R$4.664,68: 27,5%.
+O programa deve exibir o valor do imposto e o salário líquido após a dedução.
+*/
